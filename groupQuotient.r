c67f323e2f090edec724f3b01e706d42344b5c39
Andrea Fink <Poopy@andreagsmacbook.gateway.sonic.net>
2015-05-25 14:06:09 -0700

    Added some template functions.

--- groupQuotient.r
+++ groupQuotient.r
@@ -17,6 +17,34 @@ Do all analysis from transformed data.
 "
 
 # generate random group element
+pix.val = function(pic, x,y){
+    " x and y coords vary between -13 and 14
+    returns the value of the pic at the coord,
+    where stuff outside is zero."
+    if(x < -13 | x > 14 | y < -13 | y > 14){
+        result = 0
+    } else {
+        result = pic[coord2pic(x,y)]
+    }
+    return(result)
+}
+coord2pic = function(x,y){
+    return( (x + 13) * 28 + 14 + y )
+}
+pic2coord = function(z){
+    #the other way!
+}
+#two steps: 
+#1) use GL_2 to do some stuff, 
+gl.opp = function(pic, a,b,c,d){
+    #assert det != 0
+}
+#2) do a shift to move (0,0) to some other point
+shift.opp  = function(pic, x,y){
+   # return a pic 
+}
 
-
+# umm, once you have quotient space, try simple 10-means clustering
+# could I try some kind of 'gradient decent' where the motions
+# are defined by the group elements and directed by the errors?
 # backpropigate cosine errors
